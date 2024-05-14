d2782312223b631a855919b1993a5d5330cc2785
Navneet <hunnyhunnyhunny@gmail.com>
2024-05-14 14:55:00 -0400

    Added Chat Component and Reac Query

--- app/(dashboard)/chat/page.js
+++ app/(dashboard)/chat/page.js
@@ -1,5 +1,14 @@
-const ChatPage = () => {
-  return <div>ChatPage</div>;
-};
-
-export default ChatPage;
+import Chat from '@/components/Chat';
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from '@tanstack/react-query';
+export default async function ChatPage() {
+  const queryClient = new QueryClient();
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Chat />
+    </HydrationBoundary>
+  );
+}
